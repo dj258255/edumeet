7346b0340a0447bf157816e61225edc6535a0622
kwonminhwan <minhwan2315@naver.com>
2025-07-23 14:31:55 +0900

    머지수정

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -44,17 +44,7 @@ apiClient.interceptors.response.use(
 )
 
 // 인증 관련 API 함수들
-export const authAPI = {
-
-  verifycode:(verify)=>{
-    return apiClient.post("/members/verify-code",verify)
-  },
-
-  //코드 전송
-  sendcode:(email)=>{
-    return apiClient.post("/members/send-code",email)
-  },
-
+const authAPI = {
   // 회원가입
   signup: (userData) => {
     return apiClient.post("/members/signup", userData)
