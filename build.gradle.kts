a18f1b0da7dab535dc1936e8acdda721f5d1cbc4
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-07-28 06:23:57 +0900

    chore: Spring Security dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    // Spring Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
     // Swagger (OpenAPI)
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
