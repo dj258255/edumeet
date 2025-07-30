b7fd18793ed3e4398084af131596003e8ad34d81
권시온 <azzionkwon@gmail.com>
2025-07-30 12:16:09 +0900

    chore: Redis 관련 dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     // Spring Security
     implementation("org.springframework.boot:spring-boot-starter-security")
 
