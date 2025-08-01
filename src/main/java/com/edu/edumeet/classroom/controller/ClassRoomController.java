9e27b01b7aa5bb8f4f631253fb41436bc21d8d74
박시은 <psieun0606@gmail.com>
2025-08-02 01:51:42 +0900

    style: 필요없는 import문 로깅 삭제

--- src/main/java/com/edu/edumeet/classroom/controller/ClassRoomController.java
+++ src/main/java/com/edu/edumeet/classroom/controller/ClassRoomController.java
@@ -1,10 +1,9 @@
 package com.edu.edumeet.classroom.controller;
 
 import com.edu.edumeet.classroom.dto.request.ClassCreateRequestDto;
+import com.edu.edumeet.classroom.dto.response.ClassInfosResponseDto;
 import com.edu.edumeet.classroom.service.ClassService;
-import com.edu.edumeet.member.domain.Member;
 import com.edu.edumeet.member.domain.SecurityMember;
-import com.edu.edumeet.member.infrastructure.MemberJpaEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -29,7 +28,6 @@ public class ClassRoomController {
             @AuthenticationPrincipal SecurityMember member,
             @RequestBody ClassCreateRequestDto classCreateDto) {
         classService.create(member.getMemberId(), classCreateDto);
-        System.out.println(member);
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
                 "message", "클래스룸 생성이 완료되었습니다."
         ));
