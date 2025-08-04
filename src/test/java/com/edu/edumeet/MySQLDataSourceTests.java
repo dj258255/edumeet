7b2dcf53d7f25b1eb7eaaad9029513b879f75b63
범수 <dj258255@naver.com>
2025-08-04 14:15:11 +0900

    factor : import add

--- src/test/java/com/edu/edumeet/MySQLDataSourceTests.java
+++ src/test/java/com/edu/edumeet/MySQLDataSourceTests.java
@@ -4,6 +4,7 @@ package com.edu.edumeet;
 import lombok.Cleanup;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
