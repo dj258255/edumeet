57085b7b8cb78269a2beaa1c45285de73a433854
범수 <dj258255@naver.com>
2025-08-11 10:10:06 +0900

    refactor : spring security api path fix

--- src/main/java/com/edu/edumeet/config/SecurityConfig.java
+++ src/main/java/com/edu/edumeet/config/SecurityConfig.java
@@ -68,7 +68,10 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/api-docs/**",
                                 "/swagger-resources/**",
-                                "/webjars/**"
+                                "/webjars/**",
+                                "/api/v1/members/send-code",
+                                "/api/v1/members/verification",
+                                "/api/v1/members/check-email"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
